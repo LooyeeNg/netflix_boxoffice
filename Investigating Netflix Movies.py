d0397a35e74a0fe19c774265e7c7df3e36392675
LooyeeNg <64885078+LooyeeNg@users.noreply.github.com>
2022-03-12 11:48:54 +1100

    Marking non-feature films

--- Investigating Netflix Movies.py
+++ Investigating Netflix Movies.py
@@ -64,6 +64,24 @@ short_movies = netflix_movies_col_subset[netflix_movies_col_subset['duration'] <
 # Print the first 20 rows of short_movies
 short_movies.head(20)
 
+# Define an empty list
+colors = []
+
+# Iterate over rows of netflix_movies_col_subset
+for lab, row in netflix_movies_col_subset.iterrows():
+    if row['genre'] == "Children":
+        colors.append("red")
+    elif row['genre'] == "Documentaries":
+        colors.append("blue")
+    elif row['genre'] == "Stand-Up":
+        colors.append("green")
+    else:
+        colors.append("black")
+
+# Inspect the first 10 values in your list
+colors[0:10]
+
+
 
 
 
